892fc4ce8841b860b7548f97a4984d320a4ccef0
krisananthu <ananthakrishnanpta@gmail.com>
2017-01-31 14:38:42 +0530

    Added search according to subject

--- gmail.py
+++ gmail.py
@@ -27,9 +27,9 @@ for col in ws.iter_cols(min_row=1,max_col=1, max_row = 3):
     for cell in col:
         mbr = str(cell.value)
         print("%s"%(mbr))
-        """
-        #result, data = mail.search(None, '(FROM mbr)')
-        result, data = mail.search(None, '(FROM "foss-2016@googlegroups.com")')
+        
+        result, data = mail.search(None, '(SUBJECT "Weekly")')
+        #result, data = mail.search(None, '(FROM "foss-2016@googlegroups.com")')
         ids = data[0] # data is a list.
         id_list = ids.split() # ids is a space separated string
         latest_email_id = id_list[-1]#get the latest 
@@ -38,4 +38,29 @@ for col in ws.iter_cols(min_row=1,max_col=1, max_row = 3):
         email_message = email.message_from_string(raw_email)
         print email.utils.parseaddr(email_message['From'])
         print email_message.items()
-        """
+        
+        
+        
+        
+        
+        
+"""from openpyxl import Workbook
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+import datetime
+ws['A2'] = datetime.datetime.now()
+
+# Save the file
+wb.save("sample.xlsx")
+
+"""
